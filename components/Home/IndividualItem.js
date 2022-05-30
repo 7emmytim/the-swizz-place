6629c826895db85998c8e89da3d36ad218ae7ae0
Timilehin Omotugba <omotugbatimilehin7@gmail.com>
2022-05-30 19:42:28 +0100

    ui changes

--- components/Home/IndividualItem.js
+++ components/Home/IndividualItem.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { useCart } from '../../context/cart'
-import { XIcon } from '@heroicons/react/outline'
+import { InformationCircleIcon, PlusCircleIcon, XCircleIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 import { message } from 'antd'
 
@@ -20,38 +20,49 @@ const IndividualItem = ({ product }) => {
                         src={`https://immense-crag-15942.herokuapp.com${product.image.data.attributes.url}`}
                         alt='Shopping cart'
                     />
-                    {
-                        cart.find(x => x.id === product.id) ?
-                            <p
-                                onClick={() => cartAction(product, 'remove_from_cart')}
-                                className='absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group'>
-                                <XIcon className='h-6 w-6' />
-                            </p> :
-                            <p
-                                onClick={() => product.inventory.available > 0 ? cartAction(product, 'add_to_cart') : message.warn('Product is currently unavailable')}
-                                className='absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group'>
-                                <svg
-                                    xmlns='http://www.w3.org/2000/svg'
-                                    className='h-6 w-6 group-hover:opacity-50 opacity-70'
-                                    fill='none'
-                                    viewBox='0 0 24 24'
-                                    stroke='black'>
-                                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.5' d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z' />
-                                </svg>
-                            </p>
-                    }
+                    <p className='absolute right-2 top-2 bg-white rounded-full h-8 w-8 cursor-pointer group flex items-center justify-center'>
+                        {product.inventory.available}
+                    </p>
                 </div>
                 <div className='mt-4 pl-2 mb-2 flex justify-between'>
                     <div>
-                        <p className='text-md font-semibold text-gray-900 mb-0'>{product.name}</p>
-                        <p className='text-md text-red-500 mt-0'>₦{product.price.formatted}</p>
+                        <p className='font-semibold text-lg leading-tight truncate mb-0'>{product.name}</p>
                     </div>
                     <div className='flex flex-col-reverse mb-1 mr-4 group cursor-pointer'>
-                        <button className='px-3 py-1 bg-yellow-500 text-white rounded-sm text-sm remove-link-decoration'>
+                        {/* <button className='px-3 py-1 bg-yellow-500 text-white rounded-sm text-sm remove-link-decoration'>
                             <Link href={`/product/${product.id}`}>Details</Link>
-                        </button>
+                        </button> */}
+                        <p className='text-gray-600 text-sm mt-0'>₦{product.price.formatted}</p>
                     </div>
                 </div>
+
+
+                <div className='border-t border-gray-200'>
+                    <div className='flex divide-x divide-gray-200r'>
+                        <span className='block flex-1 text-center text-sm text-indigo-500 hover:text-indigo-600 font-medium px-3 py-4'>
+                            {
+                                cart.find(x => x.id === product.id) ?
+                                    <div onClick={() => cartAction(product, 'remove_from_cart')} className='flex items-center justify-center'>
+                                        <XCircleIcon className='w-5 h-5 mr-2 text-red-500' />
+                                        <span className='text-red-500'>Remove</span>
+                                    </div> :
+                                    <div onClick={() => product.inventory.available > 0 ? cartAction(product, 'add_to_cart') : message.warn('Product is currently unavailable')} className='flex items-center justify-center'>
+                                        <PlusCircleIcon className='w-5 h-5 mr-2' />
+                                        <span>Add to buy</span>
+                                    </div>
+                            }
+                        </span>
+                        <span className='block flex-1 text-center text-sm text-yellow-500 hover:text-yellow-600 font-medium px-3 py-4 group'>
+                            <div className='flex items-center justify-center'>
+                                <InformationCircleIcon className='w-5 h-5 text-yellow-400 group-hover:text-yellow-500 mr-2' />
+                                <div className='remove-link-decoration'>
+                                    <Link href={`/product/${product.id}`}>Details</Link>
+                                </div>
+                            </div>
+                        </span>
+                    </div>
+                </div>
+
             </div>
 
         </div>
